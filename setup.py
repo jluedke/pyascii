30172d7a32da1e209a6a8591691b16e68ae42a22
James M. Luedke <contact@jluedke.com>
2013-03-27 21:12:13 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+# setup.py                                                                                                  
+from distutils.core import setup
+ 
+setup(name = "pyascii",
+    version = '0.1',
+    scripts = ['pyascii'],
+    requires=[]
+)
